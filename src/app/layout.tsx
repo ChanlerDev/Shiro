df18014769e8177ce543befb5d179c39901588e6
Merryfling <merryfling@gmail.com>
2025-12-03 01:45:38 +0800

    feat: add vercel analytics and speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import '../styles/index.css'
 
 import type { PropsWithChildren } from 'react'
 
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+
 import { init } from './init'
 import { InitInClient } from './InitInClient'
 
@@ -11,6 +14,8 @@ export default async function RootLayout({ children }: PropsWithChildren) {
     <>
       {children}
       <InitInClient />
+      <Analytics />
+      <SpeedInsights />
       {/* <BrowserSupport /> */}
     </>
   )
